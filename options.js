147831d12911a4f131c623788dc91b465827104d
Philip Cali <philip.cali@gmail.com>
2012-04-03 22:09:54 -0500

    Making browser action instead

--- options.js
+++ options.js
@@ -1,33 +1,36 @@
 $(function() {
-  var host = $("#host");
-  var key = $("#key");
-
-  var deleteicon = '<img src="images/delete.png"/>';
-  var urlicon = '<img src="images/url.png"/>';
+  var host = $("#host"),
+      key = $("#key"),
+      title = $("#title"),
+      deleteicon = '<img src="images/delete.png"/>',
+      urlicon = '<img src="images/url.png"/>';
 
   var reload = function() {
     $("#html-table").html('');
-    for (var i = 0; i < localStorage.length; i++) {
-      var url = localStorage.key(i);
-
-      var del = '<a href="#" class="delete" id="'+ i +'">' + deleteicon + '</a>';
-      var get = '<a href="'+ url +'" class="get" target="_blank">'+ urlicon +'</a>';
+    for (var i = 0; i < localStorage.length; i += 3) {
+      var url = localStorage.getItem(localStorage.key(i)),
+          k = localStorage.getItem(url + '-key'),
+          t = localStorage.getItem(url + '-title'),
+          del = '<a href="#" class="delete" id="'+ i +'">' + deleteicon + '</a>',
+          get = '<a href="'+ url +'" class="get" target="_blank">'+ urlicon +'</a>';
 
       $("#host-table").append(
-        "<tr><td>"+ url +"</td><td>"+ localStorage.getItem(url) +
+        "<tr><td>"+ url +"</td><td>"+ k +"</td><td>" + t +
         "</td><td>"+ [get, del].join(' | ') +"</td></tr>"
       );
     }
   };
 
   $(".delete").live("click", function() {
-    var i = $(this).attr('id');
-    var url = localStorage.key(i);
-
-    var res = confirm("Are you sure you want to delete, " + url + "?");
+    var i = $(this).attr('id'),
+        k = localStorage.key(i),
+        url = localStorage.getItem(k),
+        res = confirm("Are you sure you want to delete, " + url + "?");
 
     if (res) {
-      localStorage.removeItem(url);
+      localStorage.removeItem(k);
+      localStorage.removeItem(url + '-key');
+      localStorage.removeItem(url + '-title');
       window.location.href = window.location.href;
     }
 
@@ -35,13 +38,17 @@ $(function() {
   });
 
   $("#add").on("click", function() {
-    var url = $(host).val();
+    var url = $(host).val(),
+        name = $(title).val() ? $(title).val() : "Vision Host";
+
     if (!url.match(/\/robot\-vision\.html$/) || $(key).val() == '') {
       alert("Please enter a valid host and key");
       return false;
     }
 
-    localStorage.setItem(url, $(key).val());
+    localStorage.setItem((new Date().getTime()) + '', url);
+    localStorage.setItem(url + '-key', $(key).val());
+    localStorage.setItem(url + '-title', name);
     window.location.href = window.location.href;
 
     return false;
---
+++ popup.js
@@ -0,0 +1,33 @@
+$(function() {
+  var icon = '<img src="images/url.png"/>';
+
+  if (localStorage.length == 0) {
+    $("ul").append(
+      '<li>Add vision hosts in the option page</li>'
+    );
+  }
+
+  for (var i=0; i < localStorage.length; i += 3) {
+    var time = localStorage.key(i),
+        url = localStorage.getItem(time),
+        title = localStorage.getItem(url + '-title');
+
+    $("ul").append(
+      '<li><a class="host" id="' + time + '" href="' + url +
+      '">' + [icon, title].join(' ') + '</a></li>'
+    );
+  }
+
+  $(".host").on('click', function() {
+    var url = localStorage.getItem($(this).attr('id'));
+
+    chrome.tabs.create({ url: url, active: true }, function(tab) {
+      var key = localStorage.getItem(url + '-key');
+          remoteCode = 'standardConnect("' + key + '");';
+
+      if (key) {
+        chrome.tabs.executeScript(tab.id, { code: remoteCode });
+      }
+    });
+  });
+});
